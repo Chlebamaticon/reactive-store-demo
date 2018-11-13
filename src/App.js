51b2caad41749bd63fc363ff6be6d6cdcd83a7c7
Dawid <dwojda@macro.local>
2018-11-13 16:26:47 +0100

    poc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { UserList } from "./UserList";
+
+function A() {
+  return <p>fdsfsdds</p>
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <UserList dupa={34}/>
+          <A />
         </header>
       </div>
     );
